ac7da62ae5a1bf774c21c110e89819e48f7989ff
金正朋也 <mhtnim1023@gmail.com>
2023-08-29 11:17:53 +0900

    Feat: add 参考情報

--- indexer/src/main/java/micce/Indexer.java
+++ indexer/src/main/java/micce/Indexer.java
@@ -2,6 +2,11 @@ package micce;
 
 public class Indexer {
     public static void main(String[] args) {
+        // fire store read
+        // 参考：https://github.com/googleapis/java-firestore/blob/main/samples/snippets/src/main/java/com/example/firestore/Quickstart.java
+
+        // vespa feed
+        // 参考: https://github.com/vespa-engine/vespa/blob/master/vespa-feed-client-api/src/test/java/ai/vespa/feed/client/examples/SimpleExample.java
         System.out.println("sss");
     }
 }
