70d4ebf5092b68d403c03f20fa8272632390680e
金正朋也 <mhtnim1023@gmail.com>
2023-10-27 00:27:38 +0900

    Feat: service.xml作成

---
+++ search-engine/package.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+ENV=$1
+if [ -d "target" ]; then
+	echo "target ディレクトリが存在します。削除します。"
+	rm -r target/
+fi
+
+echo "target ディレクトリが存在しません。作成します。"
+mkdir target
+
+cp -r components target/
+cp -r schemas target/
+cp -r $ENV/ target/
+
